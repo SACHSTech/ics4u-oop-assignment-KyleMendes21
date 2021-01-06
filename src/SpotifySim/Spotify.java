dbcd24f8fab1db3b08c3e40e5ad30bdd1fc89a13
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2021-01-06 02:26:17 +0000

    testing

--- src/SpotifySim/Spotify.java
+++ src/SpotifySim/Spotify.java
@@ -17,21 +17,33 @@ public class Spotify {
     Song Youbroke = new Song("You broke me first", "Tate McRae", "May", "New", false);
     Song Stuck = new Song("Stuck with U", "Ariana Grande ft Justin Bieber", "May", "Chilling", false);
     Song Mad = new Song("Mad at Disney", "Salem ilese", "May", "Cool Stuff", false);
-    Song  ThisCity = new Song("This City", "Sam Fischer", "June", "Cool Stuff", false);
+    Song ThisCity = new Song("This City", "Sam Fischer", "June", "Cool Stuff", false);
 
     Albums New = new Albums("New");
     Albums Chilling = new Albums("Chilling");
     Albums CoolStuff = new Albums("Cool Stuff");
 
+    System.out.println(Spirits);
+    System.out.println(Selfish);
+    System.out.println(Easy);
+    System.out.println(Lose);
+    System.out.println(Youbroke);
+    System.out.println(Stuck);
+    System.out.println(Mad);
+    System.out.println(ThisCity);
+    
+    New.addSong(Spirits);
+    Chilling.addSong(Selfish);
+    New.addSong(Easy);
+    New.addSong(Lose);
+    New.addSong(Youbroke);
+    Chilling.addSong(Stuck);
+    CoolStuff.addSong(Mad);
+    CoolStuff.addSong(ThisCity);
 
-    System.out.println();
-    //toString(Spirits);
-
-
-    Hi.addSong(Spirits);
-
-
-    System.out.println(Hi);
+    System.out.println(New);
+    System.out.println(Chilling);
+    System.out.println(CoolStuff);
 
 
   }
