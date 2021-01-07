020c60f412351879ab190c777d9b2a356a88e0ac
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2021-01-07 01:01:22 +0000

    add song comments

--- src/SpotifySim/Song.java
+++ src/SpotifySim/Song.java
@@ -4,60 +4,87 @@ import SpotifySim.Library;
 import SpotifySim.Albums;
 import SpotifySim.Playlist;
 import java.util.ArrayList;
-//import SpotifySim.Favourites;
 
 public class Song {
+
+  // instance variables
   private String name;
   private String artist;
   private String playlist;
   private String album;
   private String release;
-  //private boolean favourites;
-  //private ArrayList<Song> Favourites;
 
+  /** 
+  * Song constructor. Creates an instance of the object for Song. 
+  *
+  * @param name - a string that shows the name of the song
+  * @param artist - a string that shows the artist of the song
+  * @param playlist - a string that shows which playlist the songs belongs
+  * @param album - a string that shows which album the songs belongs
+  * @param release - a string that shows the release month of the song
+  */
   public Song(String newName, String newArtist, String newRelease, String newPlaylist, String newAlbum) {
     name = newName;
     artist = newArtist;
     release = newRelease;
     playlist = newPlaylist;
     album = newAlbum;
-    //favourites = newFavourites;
   }
 
-  /*
-
-  public void favourites(Song song, Favourites favourite) {
-    if(favourites == false) {
-      favourite.addSong(song, favourite);
-    }
-    favourite = true;
-  }
-
-  public void unfavourite() {
-    favourite = false;
-  }
+  /** 
+  * Getter method that returns the name of the song 
+  * 
+  * @return name
   */
-
   public String getName() {
     return name;
   }
 
+  /** 
+  * Getter method that returns the artist 
+  * 
+  * @return artist
+  */
   public String getArtist() {
     return artist;
   }
 
+  /** 
+  * Getter method that returns the release month 
+  * 
+  * @return release
+  */
   public String getRelease() {
     return release;
   }
 
+  /** 
+  * Getter method that returns the playlist 
+  * 
+  * @return playlist
+  */
   public String getPlaylist() {
     return playlist;
   }
 
+  /** 
+  * Getter method that returns the album 
+  * 
+  * @return album
+  */
   public String getAlbum() {
     return album;
   }
 
+  /** 
+  * toString method that returns song information 
+  *
+  * @param getName() - a string that shows the name of the song
+  * @param getArtist() - a string that shows the artist of the song
+  * @param getPlaylist() - a string that shows which playlist the songs belongs
+  * @param getAlbum() - a string that shows which album the songs belongs
+  * @param getRelease() - a string that shows the release month of the song
+  */
   public String toString() {
     if (album.equals("NONE")) {
       return getName() + " by " + getArtist() + " released in " + getRelease() + " is in the playlist " + getPlaylist();
