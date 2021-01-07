82a06bdd17e917bb98a9120b27ab56b00aedd4b5
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2021-01-07 01:06:24 +0000

    comment on album

--- src/SpotifySim/Albums.java
+++ src/SpotifySim/Albums.java
@@ -5,19 +5,37 @@ import SpotifySim.Song;
 import SpotifySim.Library;
 
 public class Albums extends Library {
-  //private String name;
+
+  // instance variables
   private String artist;
   private ArrayList<Song> Library = new ArrayList<Song>();
 
+  /** 
+  * Albums constructor. Creates an instance of the object for Albums. 
+  *
+  * @param setName - a string that shows the name of the song
+  * @param artist - a string that shows the artist of the song
+
+  */
   public Albums(String setName, String newArtist){
     super(setName);
     artist = newArtist;
   }
 
+  /** 
+  * Gettor method that returns getName() 
+  *
+  * @return getName()
+  */
   public String getName() {
     return super.getName();
   }
 
+  /** 
+  * Gettor method that returns the artist 
+  *
+  * @return the artist
+  */
   public String getArtist() {
     return artist;
   }
@@ -27,10 +45,22 @@ public class Albums extends Library {
     return Library;
   }
 
+  /** 
+  * Gettor method that returns the library array list
+  *
+  * @return library list
+  */
   public ArrayList getLibrary() {
     return Library;
   }
 
+  /** 
+  * toString method that returns album information 
+  *
+  * @param getName() - a string that shows the name of the song
+  * @param getArtist() - a string that shows the artist of the song
+  * @param Library - an ArrayList that shows all the songs in the album
+  */
   public String toString() {
     return super.getName() + " by " + artist + " " + Library;
   }
